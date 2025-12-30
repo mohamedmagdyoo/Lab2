a88a3e581f3ff88127b92900bb95c53814076120
magdy <mohamedmagdy800k@gmail.com>
2025-12-30 11:43:28 +0200

    init commit

---
+++ HelloIti.java
@@ -0,0 +1,13 @@
+class HelloIti
+{
+	public void printHelloIti()
+	{
+		 System.out.println("Hello ITI");
+	}
+
+	public void printHelloGit()
+	{
+		 System.out.println("Hello Git");
+	}
+	
+}
---
+++ HelloSwift.java
@@ -0,0 +1,7 @@
+ class HelloWorld
+ {
+	 public void printHellow
+	 {
+		 System.out.println("Hello World");
+	 }
+ }
